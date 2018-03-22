79390b3c8bbb7de2611a4a19b9e6ab5404a603f8
春田石雨 <flowke@163.com>
2018-03-22 23:00:40 +0800

    jest i

---
+++ Jest/website/languages.js
@@ -0,0 +1,23 @@
+/**
+ * Copyright 2004-present Facebook. All Rights Reserved.
+ *
+ */
+const languages = [
+  {
+    enabled: true,
+    name: 'English',
+    tag: 'en',
+  },
+
+  {
+    enabled: false,
+    name: '简体中文',
+    tag: 'zh-Hans',
+  },
+  {
+    enabled: false,
+    name: '繁體中文',
+    tag: 'zh-Hant',
+  },
+];
+module.exports = languages;
